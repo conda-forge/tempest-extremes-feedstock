42f71914d09113ee97571c7ed5dc4a00b52e110d
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-04-02 19:35:34 -0600

    Remove patches and switch to cmake build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,15 @@
 set -x
 set -e
 
-make
+# Create a build directory and navigate into it
+mkdir -p build
+cd build
 
-cp bin/* "${PREFIX}/bin"
+# Configure the build using CMake
+cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release
+
+# Build the project
+cmake --build . -- -j${CPU_COUNT}
+
+# Install the binaries
+cmake --install .
